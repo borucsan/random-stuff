b26cfe80dc34e2a3c3c61c6015fec7c3bc7836f8
dkarwot <d.karwot@sare.pl>
2020-03-25 21:03:23 +0100

    created basic apollo server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const { ApolloServer } = require('apollo-server');
+
+const server = new ApolloServer({
+    typeDefs: `
+        type Query {
+            greeting: String
+        }
+    `
+});
+
+server.listen({port: 4567}).then((result) => console.log(result.url));
