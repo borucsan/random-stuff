bc27f9f73a39a497c9e48eb1eaabd95bcc384a24
dkarwot <d.karwot@sare.pl>
2020-03-26 22:27:00 +0100

    added float and boolean test values added DaysOfWeek enum added workDays value to server

--- index.js
+++ index.js
@@ -10,15 +10,32 @@ const server = new ApolloServer({
             email: String
             pets: [String]
             randomNumbers: [Int]
+            amWorking: Boolean
+            pi: Float,
+            workDays: [DayOfWeek]
+        }
+        enum DayOfWeek {
+            MON
+            TUE
+            WED
+            THU
+            FRI
+            SAT
+            SUN
         }
     `,
     rootValue: () => {
+        const now = new Date();
+        const DAYS_OF_WEEK = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
         return {
             interestingUrls: ["https://smashingmagazine.com", "https://twitter.com/garrows/status/1065217184643768320"],
             firstName: 'John',
             email: 'john@example.com',
             pets: ['Mittens', 'Doggo', 'Birb'],
-            randomNumbers: Array.from({length: NUMBERS_COUNT}, () => Math.floor(Math.random() * 10000))
+            randomNumbers: Array.from({length: NUMBERS_COUNT}, () => Math.floor(Math.random() * 10000)),
+            amWorking: [2,3].includes(now.getDay()) || now.getHours() < 12,
+            pi: Math.PI,
+            workDays: DAYS_OF_WEEK.slice(1, 6),
         }
     },
     playground: true,
