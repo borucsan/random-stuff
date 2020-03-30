941955fb4fd258be9d8656e631acbadd5f88c195
dkarwot <d.karwot@sare.pl>
2020-03-30 22:21:50 +0200

    added euler number and series

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
-
+const EULERSERIES_COUNT = 1000;
 const NUMBERS_COUNT = 10;
 
 const server = new ApolloServer({
@@ -13,6 +13,8 @@ const server = new ApolloServer({
             amWorking: Boolean
             pi: Float,
             workDays: [DayOfWeek]
+            e: Float
+            eulersSeries: [Float]
         }
         enum DayOfWeek {
             MON
@@ -36,6 +38,8 @@ const server = new ApolloServer({
             amWorking: [2,3].includes(now.getDay()) || now.getHours() < 12,
             pi: Math.PI,
             workDays: DAYS_OF_WEEK.slice(1, 6),
+            e: Math.E,
+            eulersSeries: Array.from({length: EULERSERIES_COUNT}, (v, k) => k + 1 ).map(n => Math.pow((1 + 1/n), n)) 
         }
     },
     playground: true,
