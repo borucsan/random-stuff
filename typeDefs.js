878908fa214514ee580047f6b36a2300169216b8
dkarwot <d.karwot@sare.pl>
2020-04-06 21:14:14 +0200

    added docs to typeDef

--- typeDefs.js
+++ typeDefs.js
@@ -2,16 +2,49 @@ const { gql } = require('apollo-server')
 
 module.exports = gql`
   type Query {
+    """
+    Interesting url to visit
+    """
     interestingUrls: [String]
+    """
+    Test name
+    """
     firstName: String
+    """
+    Test email
+    """
     email: String
+    """
+    Test array of pets names
+    """
     pets: [String]
+    """
+    Array of random numbers
+    """
     randomNumbers: [Int]
+    """
+    Check if I am working now
+    """
     amWorking: Boolean
+    """
+    PI  number
+    """
     pi: Float
+    """
+    List of work days
+    """
     workDays: [DayOfWeek]
+    """
+    Euler number
+    """
     e: Float
+    """
+    Euler series array 
+    """
     eulersSeries: [Float]
+    """
+    Current time in binary
+    """
     timeBinary: TimeBinary!
   }
   enum DayOfWeek {
